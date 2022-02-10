489b52eb02581680162b5d0c64d71c96454114c4
Brad Howes <bradhowes@mac.com>
2022-02-10 17:27:26 +0100

    Simplified

--- iOS/Extension/ViewController.swift
+++ iOS/Extension/ViewController.swift
@@ -21,7 +21,6 @@ extension Knob: AUParameterValueProvider, RangedControl {}
 
   private let parameters = AudioUnitParameters()
   private var viewConfig: AUAudioUnitViewConfiguration!
-  private var keyValueObserverToken: NSKeyValueObservation?
 
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var controlsView: View!
@@ -61,6 +60,22 @@ extension Knob: AUParameterValueProvider, RangedControl {}
   @IBOutlet weak var odd90Control: Switch!
   @IBOutlet weak var negativeFeedbackControl: Switch!
 
+  private lazy var controls: [ParameterAddress: [(Knob, Label, UIView)]] = [
+    .depth: [(depthControl, depthValueLabel, depthTapEdit),
+             (altDepthControl, altDepthValueLabel, altDepthTapEdit)],
+    .delay: [(delayControl, delayValueLabel, delayTapEdit),
+             (altDelayControl, altDelayValueLabel, altDelayTapEdit)],
+    .rate: [(rateControl, rateValueLabel, rateTapEdit)],
+    .feedback: [(feedbackControl, feedbackValueLabel, feedbackTapEdit)],
+    .wet: [(wetMixControl, wetMixValueLabel, wetMixTapEdit)],
+    .dry: [(dryMixControl, dryMixValueLabel, dryMixTapEdit)]
+  ]
+
+  private lazy var switches: [ParameterAddress: Switch] = [
+    .odd90: odd90Control,
+    .negativeFeedback: negativeFeedbackControl
+  ]
+
   // Holds all of the other editing views and is used to end editing when tapped.
   @IBOutlet weak var editingView: View!
   // Background that contains the label and value editor field. Always appears just above the keyboard view.
@@ -79,90 +94,79 @@ extension Knob: AUParameterValueProvider, RangedControl {}
   // constrained width layouts.
   private var editors = [ParameterAddress : [AUParameterEditor]]()
 
-  private lazy var pairs: [ParameterAddress: [(Knob, Label)]] = [
-    .depth: [(depthControl, depthValueLabel),
-             (altDepthControl, altDelayValueLabel)],
-    .delay: [(delayControl, delayValueLabel),
-             (altDelayControl, altDelayValueLabel)],
-    .rate: [(rateControl, rateValueLabel)],
-    .feedback: [(feedbackControl, feedbackValueLabel)],
-    .wet: [(wetMixControl, wetMixValueLabel)],
-    .dry: [(dryMixControl, dryMixValueLabel)]
-  ]
-
-  private lazy var switches: [ParameterAddress: Switch] = [
-    .odd90: odd90Control,
-    .negativeFeedback: negativeFeedbackControl
-  ]
-
   public var audioUnit: FilterAudioUnit? {
     didSet {
       DispatchQueue.main.async {
         if self.isViewLoaded {
-          self.connectViewToAU()
+          self.createEditors()
         }
       }
     }
   }
+}
 
-  public override func viewDidLoad() {
+public extension ViewController {
+
+  override func viewDidLoad() {
     os_log(.info, log: log, "viewDidLoad BEGIN")
-
     super.viewDidLoad()
 
     view.backgroundColor = .black
     if audioUnit != nil {
-      connectViewToAU()
+      createEditors()
     }
-
-    editingViewTopConstraint.constant = 0
-    editingBackgroundBottomConstraint.constant = view.frame.midY
-
-    NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardAppearing(_:)),
-                                           name: UIApplication.keyboardWillShowNotification, object: nil)
-    NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardDisappearing(_:)),
-                                           name: UIApplication.keyboardWillHideNotification, object: nil)
-
-    editingBackground.layer.cornerRadius = 8.0
-    editingView.isHidden = true
-
-    addTapGesture(depthTapEdit)
-    addTapGesture(delayTapEdit)
-    addTapGesture(rateTapEdit)
-    addTapGesture(feedbackTapEdit)
-    addTapGesture(dryMixTapEdit)
-    addTapGesture(wetMixTapEdit)
-    addTapGesture(altDepthTapEdit)
-    addTapGesture(altDelayTapEdit)
-
-    os_log(.info, log: log, "viewDidLoad BEGIN")
   }
+}
+
+// MARK: - AudioUnitViewConfigurationManager
+
+extension ViewController: AudioUnitViewConfigurationManager {}
+
+// MARK: - AUAudioUnitFactory
+
+extension ViewController: AUAudioUnitFactory {
+  @objc public func createAudioUnit(with componentDescription: AudioComponentDescription) throws -> AUAudioUnit {
+    let kernel = KernelBridge(Bundle.main.auBaseName, maxDelayMilliseconds: parameters[.delay].maxValue)
+    let audioUnit = try FilterAudioUnitFactory.create(componentDescription: componentDescription,
+                                                      parameters: parameters, kernel: kernel,
+                                                      viewConfigurationManager: self)
+    self.audioUnit = audioUnit
+    return audioUnit
+  }
+}
+
+// MARK: - Private
+
+extension ViewController {
 
   private func createEditors() {
     os_log(.info, log: log, "createEditors BEGIN")
 
     let knobColor = UIColor(named: "knob")!
 
-    for (parameterAddress, pairs) in pairs {
-      var editors = [AUParameterEditor]()
-      for (knob, label) in pairs {
+    let valueEditor = ValueEditor(containerView: editingView, backgroundView: editingBackground,
+                                  parameterName: editingLabel, parameterValue: editingValue,
+                                  containerViewTopConstraint: editingViewTopConstraint,
+                                  backgroundViewBottomConstraint: editingBackgroundBottomConstraint,
+                                  controlsView: controlsView)
 
+    for (parameterAddress, pairs) in controls {
+      var editors = [AUParameterEditor]()
+      for (knob, label, tapEdit) in pairs {
         knob.progressColor = knobColor
         knob.indicatorColor = knobColor
 
-        if parameterAddress == .wet || parameterAddress == .dry {
-          knob.trackLineWidth = 8
-          knob.progressLineWidth = 6
-          knob.indicatorLineWidth = 6
-        } else {
-          knob.trackLineWidth = 10
-          knob.progressLineWidth = 8
-          knob.indicatorLineWidth = 8
-        }
+        let trackWidth: CGFloat = parameterAddress == .dry || parameterAddress == .wet ? 8 : 10
+        let progressWidth = trackWidth - 2.0
+        knob.trackLineWidth = trackWidth
+        knob.progressLineWidth = progressWidth
+        knob.indicatorLineWidth = progressWidth
 
-        editors.append(FloatParameterEditor(parameter: parameters[parameterAddress],
-                                            formatter: parameters.valueFormatter(parameterAddress),
-                                            rangedControl: knob, label: label))
+        let editor = FloatParameterEditor(parameter: parameters[parameterAddress],
+                                          formatter: parameters.valueFormatter(parameterAddress),
+                                          rangedControl: knob, label: label)
+        editors.append(editor)
+        editor.setValueEditor(valueEditor: valueEditor, tapToEdit: tapEdit)
       }
       self.editors[parameterAddress] = editors
     }
@@ -171,47 +175,10 @@ extension Knob: AUParameterValueProvider, RangedControl {}
     for (parameterAddress, control) in switches {
       os_log(.info, log: log, "createEditors - before BooleanParameterEditor")
       editors[parameterAddress] = [BooleanParameterEditor(parameter: parameters[parameterAddress],
-                                                         booleanControl: control)]
+                                                          booleanControl: control)]
     }
 
     os_log(.info, log: log, "createEditors END")
-  }
-
-  @IBAction func handleKeyboardAppearing(_ notification: NSNotification) {
-    os_log(.info, log: log, "handleKeyboardAppearing BEGIN")
-
-    guard let info = notification.userInfo else {
-      os_log(.error, log: log, "handleKeyboardAppearing END - no userInfo dict")
-      return
-    }
-
-    guard let keyboardFrameEnd = info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
-      os_log(.error, log: log, "handleKeyboardAppearing END - no userInfo entry for %{public}s",
-             UIResponder.keyboardFrameEndUserInfoKey)
-      return
-    }
-
-    let keyboardFrame = keyboardFrameEnd.cgRectValue
-    let localKeyboardFrame = view.convert(keyboardFrame, from: view.window)
-    os_log(.info, log: log, "handleKeyboardAppearing - height: %f", localKeyboardFrame.height)
-
-    view.layoutIfNeeded()
-    UIView.animate(withDuration: 0.4, delay: 0.0) {
-      if localKeyboardFrame.height < 100 {
-        self.editingBackgroundBottomConstraint.constant = self.view.frame.midY
-      } else {
-        self.editingBackgroundBottomConstraint.constant = localKeyboardFrame.height + 20
-      }
-      self.view.layoutIfNeeded()
-    }
-  }
-
-  @IBAction func handleKeyboardDisappearing(_ notification: NSNotification) {
-    view.layoutIfNeeded()
-    UIView.animate(withDuration: 0.4, delay: 0.0) {
-      self.editingBackgroundBottomConstraint.constant = self.view.frame.midY
-      self.view.layoutIfNeeded()
-    }
   }
 
   @IBAction public func handleKnobValueChange(_ control: Knob) {
@@ -244,158 +211,5 @@ extension Knob: AUParameterValueProvider, RangedControl {}
     (editors[address] ?? []).forEach { $0.controlChanged(source: control) }
 
     os_log(.debug, log: log, "controlChanged END")
-  }
-}
-
-extension ViewController: AUAudioUnitFactory {
-
-  /**
-   Create a new FilterAudioUnit instance to run in an AVu3 container.
-
-   - parameter componentDescription: descriptions of the audio environment it will run in
-   - returns: new FilterAudioUnit
-   */
-  public func createAudioUnit(with componentDescription: AudioComponentDescription) throws -> AUAudioUnit {
-    os_log(.info, log: log, "createAudioUnit BEGIN - %{public}s", componentDescription.description)
-
-    let kernel = KernelBridge(Bundle.main.auBaseName, maxDelayMilliseconds: parameters[.delay].maxValue)
-    let audioUnit = try FilterAudioUnit(componentDescription: componentDescription, options: [.loadOutOfProcess])
-    self.audioUnit = audioUnit
-    audioUnit.configure(parameters: parameters, kernel: kernel)
-
-    os_log(.info, log: log, "createAudioUnit END")
-    return audioUnit
-  }
-}
-
-extension ViewController {
-
-  private func connectViewToAU() {
-    os_log(.info, log: log, "connectViewToAU")
-    guard let audioUnit = audioUnit else { fatalError("logic error -- nil audioUnit value") }
-
-    createEditors()
-
-    keyValueObserverToken = audioUnit.observe(\.allParameterValues) { [weak self] _, _ in
-      guard let self = self else { return }
-      DispatchQueue.main.async {
-        if audioUnit.currentPreset != nil {
-          self.updateDisplay()
-        }
-      }
-    }
-
-    // Let us manage view configuration changes
-    audioUnit.viewConfigurationManager = self
-  }
-
-  private func updateDisplay() {
-    os_log(.info, log: log, "updateDisplay")
-    for address in ParameterAddress.allCases {
-      (editors[address] ?? []).forEach { $0.parameterChanged() }
-    }
-  }
-}
-
-extension ViewController: AudioUnitViewConfigurationManager {
-
-  public func supportedViewConfigurations(_ available: [AUAudioUnitViewConfiguration]) -> IndexSet {
-    var indexSet = IndexSet()
-    for (index, viewConfiguration) in available.enumerated() {
-      if viewConfiguration.width > 0 && viewConfiguration.height > 0 {
-        indexSet.insert(index)
-      }
-    }
-    return indexSet
-  }
-
-  public func selectViewConfiguration(_ viewConfiguration: AUAudioUnitViewConfiguration) {
-
-  }
-}
-
-extension ViewController: UITextFieldDelegate {
-
-  @IBAction func beginEditing(sender: UITapGestureRecognizer) {
-    guard editingView.isHidden,
-          let view = sender.view,
-          let address = ParameterAddress(rawValue: UInt64(view.tag)),
-          let param = editors[address]?.first?.parameter
-    else {
-      return
-    }
-
-#if targetEnvironment(macCatalyst)
-    editingBackgroundBottomConstraint = view.frame.midY
-#endif
-
-    os_log(.info, log: log, "beginEditing - %d", view.tag)
-    editingView.tag = view.tag
-    editingLabel.text = param.displayName
-    editingValue.text = "\(param.value)"
-    editingValue.becomeFirstResponder()
-    editingValue.delegate = self
-
-    editingView.alpha = 0.0
-    editingView.isHidden = false
-
-    os_log(.info, log: log, "starting animation")
-    UIView.animate(withDuration: 0.4, delay: 0.0, options: [.curveEaseIn]) {
-      self.controlsView.alpha = 0.40
-      self.editingView.alpha = 1.0
-    } completion: { _ in
-      self.controlsView.alpha = 0.40
-      os_log(.info, log: self.log, "done animation")
-    }
-  }
-
-  private func endEditing() {
-    guard let address = ParameterAddress(rawValue: UInt64(editingView.tag)) else { fatalError() }
-    os_log(.info, log: log, "endEditing - %d", editingView.tag)
-
-    editingValue.resignFirstResponder()
-
-    os_log(.info, log: log, "starting animation")
-    UIView.animate(withDuration: 0.4, delay: 0.0, options: [.curveEaseIn]) {
-      self.editingView.alpha = 0.0
-      self.controlsView.alpha = 1.0
-    } completion: { _ in
-      self.editingView.alpha = 0.0
-      self.controlsView.alpha = 1.0
-      self.editingView.isHidden = true
-      if let stringValue = self.editingValue.text,
-         let value = Float(stringValue) {
-        (self.editors[address] ?? []).forEach { $0.setEditedValue(value) }
-      }
-      os_log(.info, log: self.log, "done animation")
-    }
-  }
-
-  override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    if !editingView.isHidden {
-      endEditing()
-    }
-    super.touchesBegan(touches, with: event)
-  }
-
-  private func addTapGesture(_ view: UIView) {
-    let gesture = UITapGestureRecognizer(target: self, action: #selector(beginEditing))
-    gesture.numberOfTouchesRequired = 1
-    gesture.numberOfTapsRequired = 1
-    view.addGestureRecognizer(gesture)
-    view.isUserInteractionEnabled = true
-  }
-
-  public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-    os_log(.info, log: log, "textFieldShouldReturn")
-    endEditing()
-    return false
-  }
-
-  public func textFieldDidEndEditing(_ textField: UITextField) {
-    os_log(.info, log: log, "textFieldDidEndEditing")
-    if textField.isFirstResponder {
-      endEditing()
-    }
   }
 }
